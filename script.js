5cb197feb51e4f26650813a3cb4dc839620d242a
“davv78” <octaviandavaputra@gmail.com>
2025-04-01 19:42:42 +0700

    sorry my dear

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.getElementById("angry-btn").addEventListener("click", function () {
 
 // Fungsi untuk mengirim pesan ke WhatsApp kamu
 function sendWhatsApp() {
-    const messageText = "Sayang, aku maafin kamuu. aku harap kamu engga kayak gitu lagi yaah 😊💖";
+    const messageText = "Sayang, aku maafin kamuu. aku harap kamu engga kayak gitu lagi yaah sayaang";
     const phoneNumber = "6285748437032"; // Ganti dengan nomor WA kamu
     const encodedMessage = encodeURIComponent(messageText);
     const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
